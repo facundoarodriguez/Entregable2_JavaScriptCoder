3a0d8804562fba66ec80e1300d507ae43562a32a
Facundo Rodriguez <fakurdzz@gmail.com>
2024-09-23 11:39:14 -0300

    fetch y estilos

--- main.js
+++ main.js
@@ -20,8 +20,13 @@ registerButton.addEventListener('click', (event) => {
     
     const user = username.value;
     const pass = password.value;
+
+    const storedUsername = localStorage.getItem('username');
     
-    if (user && pass) {
+    //Validar usuario
+    if (storedUsername === user) {
+        showNotification('Ya hay un usuario registrado con este nombre.', 'error');
+    } else if (user && pass) {
         localStorage.setItem('username', user);
         localStorage.setItem('password', pass);
         showNotification('Registro exitoso! Ahora puedes iniciar sesión.', 'success');
@@ -30,7 +35,7 @@ registerButton.addEventListener('click', (event) => {
     }
 });
 
-// Función de inicio de sesión
+//Inicio de sesión
 loginButton.addEventListener('click', (event) => {
     event.preventDefault();
     
@@ -50,173 +55,9 @@ loginButton.addEventListener('click', (event) => {
     }
 });
 
-// Animación del cursor (opcional)
+
+// Animación de texto uwu
 const cursor = document.getElementById('cursor');
 setInterval(() => {
     cursor.classList.toggle('visible');
 }, 500);
-
-
-
-/* function preentregable1() {
-    let i = 0
-    let inicio = confirm ('Primero debe registrarse para iniciar el cuestionario.')
-        if (inicio == true) {
-            let c = 0
-            //C es el contador de inicios de sesion
-            let register = prompt ('Ingrese su nuevo usuario. El mismo será requerido para iniciar sesión en el futuro.')
-            if (register === null) {
-                inicio = false;
-            }
-            else if (register == ''){
-                inicio = false;
-            }
-            else {
-                alert('Usuario creado con éxito.')
-                while (c<3 && i<1) {
-                    let user = prompt ('Ingrese su usuario.')
-                    if (user === null) {
-                        i = 1;
-                    }
-                    else {
-                        if(user == register) {
-                            cuestionario(register);
-                            i++;
-                        }
-                        else {
-                            alert('El usuario no coincide.')
-                            c++;
-                        }
-                    }
-                }
-                if (c == 3){
-                    alert('Se ha alcanzado el número máximo de intentos. Sesión cerrada.');
-                }
-                if (i == 1){
-                    alert('Adiós :)');
-                }
-            }
-        }
-        else { alert('Hasta luego :)')}
-
-        //Función cuestionario
-        function cuestionario(nombre){
-            let playerScore = 0
-            alert('Bienvenido al cuestionario de preguntas de Game of Thrones')
-
-            let R1 = prompt('¿Quién es el autor de los libros en los que se basa la serie "Game of Thrones"? George R. R. ...')
-            if (R1 === null) {
-                return;
-            }
-            else {
-                if (R1.toUpperCase() == 'MARTIN') {
-                    alert('Correcto!')
-                    playerScore=playerScore + 100
-                }
-                else if (R1 == '') {
-                    alert('Sin respuesta. Incorrecto')
-                }
-                else {
-                    alert('Incorrecto')
-                }
-            }
-
-            let R2 = prompt('¿En qué continente ficticio se desarrolla la mayor parte de la historia de "Game of Thrones"?')
-            if (R2 === null) {
-                return;
-            }
-            else {
-                switch (R2.toUpperCase()) {
-                    case 'WESTEROS':
-                        alert('Correcto!')
-                        playerScore=playerScore + 100
-                        break;
-                    case 'PONIENTE':
-                        alert('Correcto!')
-                        playerScore=playerScore + 100
-                        break;
-                    case '':
-                        alert('Sin respuesta. Incorrecto')
-                        break;
-                    default:
-                        alert('Incorrecto')
-                        break;
-                }
-            }
-            
-            let R3 = prompt('¿Cuántos dragones tenía Daenerys Targaryen al principio de la serie? Completar solo con números.')
-            if (R3 == 3) {
-                alert('Correcto!')
-                playerScore=playerScore + 100
-            }
-            else if (R3 == '') {
-                alert('Sin respuesta. Incorrecto')
-            }
-            else if (R3 === null) {
-                return;
-            }
-            else {
-                alert('Incorrecto')
-            }
-
-            let R4 = prompt('¿Qué Lannister es conocido como "El Matarreyes"?')
-            if (R4 === null) {
-                return;
-            }
-            else {
-                if (R4.toUpperCase() == 'JAIME') {
-                    alert('Correcto!')
-                    playerScore=playerScore + 100
-                }
-                else if (R4 == '') {
-                    alert('Sin respuesta. Incorrecto')
-                }
-                else {
-                    alert('Incorrecto')
-                }
-            }
-
-            let R5 = prompt('¿Cuál es el verdadero nombre de Jon Snow?')
-            if (R5 ===  null) {
-                return;
-            }
-            else {
-                if (R5.toUpperCase() == 'AEGON') {
-                    alert('Correcto!')
-                    playerScore=playerScore + 100
-                }
-                else if (R5 == '') {
-                    alert('Sin respuesta. Incorrecto')
-                }
-                else {
-                    alert('Incorrecto')
-                }
-            }
-
-            //Array de usuario
-            const usuario = [ 
-                {
-                    name: "facundo",
-                    score: 300
-                },
-                {
-                    name: "javier",
-                    score: 400
-                },
-                {
-                    name: "mariano",
-                    score: 200
-                }
-            ]
-
-            let lastPlayer = {
-                name: nombre,
-                score: playerScore
-            }
-            alert('Felicidades ' + lastPlayer.name + ', haz completado el cuestionario. Tu puntaje es ' + lastPlayer.score)
-
-            usuario.push(lastPlayer)
-            usuario.sort((a, b)=> b.score - a.score);
-            console.table(usuario)
-        }
-} */
--- market.js
+++ market.js
@@ -1,12 +1,89 @@
-let cart = [];
+//Productos
+const products = [
+  { name: 'Remera', price: 4.99, image: '../images/1.png' },
+  { name: 'Hoodie', price: 13.99, image: '../images/2.png' },
+  { name: 'Gorro', price: 2.99, image: '../images/3.png' },
+  { name: 'Taza', price: 1.99, image: '../images/4.png' },
+  { name: 'Bufanda', price: 1.99, image: '../images/5.png' },
+  { name: 'Mochila', price: 14.99, image: '../images/6.png' }
+];
+
+const productContainer = document.querySelector('.contenedorMarket');
+
+//API de dolar blue al día
+fetch("https://dolarapi.com/v1/dolares/blue")
+  .then(response => response.json())
+  .then(data => {
+    const compra = data.compra;
+    const venta = data.venta;
+
+    document.getElementById('compraBlue').textContent = `${compra}`
+    document.getElementById('ventaBlue').textContent = `${venta}`
+  })
+  .catch(error => {
+    console.error(Swal.fire({
+      title: 'Algo salió mal...',
+      html: '<p class="swaltext">Error al cargar los datos</p>',
+      icon: 'error',
+      showConfirmButton: true,
+      confirmButtonText: 'Ok',
+      confirmButtonColor: '#273746',
+      timer: 5000,
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      allowEscapeKey: true,
+      alowwEnterKey: true,
+      background: '#fff',
+      customClass: {
+        title: 'swaltitle',
+      }
+    }), error);
+  });
+
+//Función para cargar los productos a la página
+function renderProducts() {
+  products.forEach(product => {
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+    productDiv.setAttribute('data-name', product.name);
+    productDiv.setAttribute('data-price', product.price);
+
+    const img = document.createElement('img');
+    img.src = product.image;
+    img.alt = product.name;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = product.name;
+
+    const p = document.createElement('p');
+    p.textContent = `$${product.price}`;
+
+    const button = document.createElement('button');
+    button.textContent = 'Agregar al carrito';
+    button.classList.add('add-to-cart');
+
+    productDiv.appendChild(img);
+    productDiv.appendChild(h3);
+    productDiv.appendChild(p);
+    productDiv.appendChild(button);
+    productContainer.appendChild(productDiv);
+  });
+}
+
+//LLamada de la función
+renderProducts();
+
+//Carrito de compras
+let cart = []; 
+let total = 0;
 
 function updateCartUI() {
   const cartItemsContainer = document.getElementById('cart-items');
   const cartTotalElement = document.getElementById('cart-total');
   cartItemsContainer.innerHTML = '';
 
-  let total = 0;
-
+  total = 0;
+  
   cart.forEach((item, index) => {
     const li = document.createElement('li');
     li.textContent = `${item.name} - $${item.price} x ${item.quantity}`;
@@ -68,69 +145,62 @@ function showNotification(message) {
 
 const btnPay = document.getElementById('pay');
 
+// Evento para el botón de pagar
 btnPay.addEventListener('click', () => {
-  const noHaceNada = document.createElement('p');
-  noHaceNada.textContent = 'No hace nada jeje';
-  btnPay.after(noHaceNada);
-  setTimeout(() => {
-    noHaceNada.remove();}, 2000);
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* let persona = 0
-let personaJSON = 0
-
-function Persona(nombre, apellido, edad) {
-    this.nombre = nombre,
-    this.apellido = apellido,
-    this.edad = edad
-    this.mostrarDatos = function() {
-        console.log('Nombre:' +persona.nombre);
-        console.log('Apellido:' +persona.apellido);
-        console.log('Edad:' +persona.edad)
-    }
-}
-
-function pedirDatos(){
-    let nombreUsuario = prompt('Ingrese su nombre:')
-    let apellidoUsuario = prompt('Ingrese su apellido:')
-    let edadUsuario = parseInt(prompt('Ingrese su edad:'))
-
-    persona = new Persona(nombreUsuario, apellidoUsuario, edadUsuario);
-
-    /* persona.mostrarDatos(); */
-
-/*   localStorage.setItem('Persona', JSON.stringify(persona));
-}
-
-function convertirDatos(){
-    personaJSON = localStorage.getItem('persona')
-    persona = JSON.parse(personaJSON)
-    persona.mostrarDatos();
-} */ 
+  if (total === 0) {
+    Swal.fire({
+      title: 'Tu carrito está vacío!',
+      html: '<p class="swaltext">Debes seleccionar al menos un producto</p>',
+      icon: 'error',
+      showConfirmButton: true,
+      confirmButtonText: 'Ok',
+      confirmButtonColor: '#273746',
+      timer: 5000,
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      allowEscapeKey: true,
+      alowwEnterKey: true,
+      background: '#fff',
+      customClass: {
+        title: 'swaltitle',
+      }
+    });
+  } else {
+    Swal.fire({
+      title: 'Confirmar comprar',
+      html: `<p class="swaltext">Estas por realizar un pago de <span class="swaltext2">$${total}</span> por los productos seleccionados</p>`,
+      icon: 'question',
+      allowOutsideClick: false,
+      allowEscapeKey: true,
+      alowwEnterKey: true,
+      showCancelButton: true,
+      confirmButtonText: 'Confirmar comprar',
+      confirmButtonColor: '#273746',
+      cancelButtonText: 'Cancelar',
+      background: '#fff',
+      customClass: {
+        title: 'swaltitle',
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Compra exitosa!',
+          html: '<p class="swaltext">Muchas gracias por adquirir nuestros productos</p>',
+          icon: 'success',
+          timer: 5000,
+          timerProgressBar: true,
+          allowOutsideClick: false,
+          allowEscapeKey: true,
+          alowwEnterKey: true,
+          confirmButtonColor: '#273746',
+          customClass: {
+            title: 'swaltitle',
+          }
+        }).then(() => {
+          cart = [];
+          updateCartUI();
+        });
+      }
+    });
+  }
+});
